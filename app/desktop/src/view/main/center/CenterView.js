ca83a888304427f43e908be3421d902b74472394
ext-js10 <learnuserext10@gmail.com>
2021-08-20 20:31:21 +0530

    first commit

--- app/desktop/src/view/main/center/CenterView.js
+++ app/desktop/src/view/main/center/CenterView.js
@@ -1,8 +1,58 @@
+/*
+var store = Ext.create('Ext.data.Store', {
+    fields: ['name','progress','result'],
+    data: [
+        { name: 'Test 1', progress: 0.10 ,result:'fail'},
+        { name: 'Test 2', progress: 0.23,result:'pass' },
+        { name: 'Test 3', progress: 0.86,result:'pass' },
+        { name: 'Test 4', progress: 0.31,result:'pass' }
+    ]
+});
+
+var grid1=Ext.create({
+    xtype: 'grid',
+    title: 'Widget Column Demo',
+    store: store,
+
+    columns: [{
+        text: 'Test Number',
+        dataIndex: 'name',
+        width: 100
+    }, {
+        //xtype: 'widgetcolumn',
+        text: 'Progress',
+        width: 120,
+        dataIndex: 'progress',
+        widget: {
+            xtype: 'progressbarwidget',
+            textTpl: '{value:percent}'
+        }
+    } ,
+	{
+		text:'Result',
+		width: 120,
+		dataIndex:'result',
+		widget: {
+            xtype: 'resultbarwidget',
+            textTpl: '{value:result}'
+        }
+
+	}
+],
+
+    width: 220,
+    height: 250,
+    renderTo: document.body
+});
+*/
+
 Ext.define('Myapp.view.main.center.CenterView', {
 	extend: 'Ext.Container',
 	xtype: 'centerview',
 	cls: 'centerview',
 	layout: 'card',
+	//items:[grid1]
+	/*
 	items:[{
 		xtype:'panel',
 		viewModel: {
@@ -35,18 +85,19 @@ Ext.define('Myapp.view.main.center.CenterView', {
 		msgTarget:'under',
 		invalidText:'"{0}" bad. "{1}" good.'
 	},
-	
+	/*
 	{
 		xtype:'button',
 		text:'Submit',
 		handler:function(){
 			console.log('Added');
 		}
-	},
+	}, */
 
+/*
 	{
 		xtype:'button',
-		text:'Save',
+		text:'Submit',
 		handler: function(){
 			console.log('Test');
 			Ext.Ajax.request({
@@ -84,6 +135,7 @@ Ext.define('Myapp.view.main.center.CenterView', {
 		}
 	},
 	*/
+	/*
 	{
 		xtype:'button',
 		text:'Save',
@@ -108,5 +160,5 @@ Ext.define('Myapp.view.main.center.CenterView', {
 	
 
 ]
-	}]
+	}] */
 });
--- app/desktop/src/view/main/detail/DetailView.js
+++ app/desktop/src/view/main/detail/DetailView.js
@@ -1,13 +1,41 @@
+
+
+// The data server is hosted on Heroku, and it doesn't start up
+// until the first request.
+Ext.define('MyApp.view.Main', {
+  extend: 'Ext.grid.Grid',
+  title: 'Businesses',
+   
+  columns: [
+      { xtype: 'rownumberer', width: 55},
+      { text: 'Name',  dataIndex: 'name', flex : 2},
+      { text: 'Address', dataIndex: 'full_address', flex : 3  },
+      { text: 'City', dataIndex: 'city', flex: 1 }
+  ],
+   
+  store: {
+      type: 'virtual',
+      pageSize: 200,
+      proxy : { type : 'ajax', url : '//nameless-tundra-27404.herokuapp.com/go/?fn=bigdata', reader : { type : 'json', rootProperty : 'data' } },
+      autoLoad: true
+  }
+   
+  });
+  Ext.application({
+  name: 'MyApp',
+  mainView: 'MyApp.view.Main'
+  });
+
 Ext.define('Myapp.view.main.detail.DetailView', {
 	extend: 'Ext.Container',
 	xtype: 'detailview',
   cls: 'detailview',
   layout: 'fit',
-  items: [
+  /*items: [
     {
       xtype: 'container', 
       style: 'background:white', 
       html: '<div style="padding:10px;font-size:24px;">detailview</div>'
     }
-  ]
+  ] */
 })
